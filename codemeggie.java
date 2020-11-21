5a18e764e59f41147b83113865ba29c21fdcc581
Your Name <robostudent@loyla.ca>
2020-11-21 17:26:36 -0500

    meggie's commit

---
+++ codemeggie.java
@@ -0,0 +1,7 @@
+public class Code {
+  public static void main(String[] args) {
+    String full_name = "Ur" + "Mom";
+    String upper_case = full_name.toUpperCase();
+    System.out.println(upper_case);
+  }
+}
