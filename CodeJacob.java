7769b17015cc8efa4b6702dcd4c11e52d8be1094
Jacob Khalifa <robostudent@loyola.ca>
2020-11-21 17:37:24 -0500

    Jacob's commit

---
+++ CodeJacob.java
@@ -0,0 +1,11 @@
+public class Code {
+  public static void main(String[] args) {
+    int Voltage = 10;
+    int Current = 10;
+    int total = 0;
+
+  Voltage * Current = total;
+
+      System.out.println(total);
+  }
+}
